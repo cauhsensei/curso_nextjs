3e92d1a9f4c0974fb2ff4184c6ebf9d6e0364ba4
Cauane Cardoso <cauhcdz@gmail.com>
2023-10-26 11:20:00 -0300

    criando rotas privadas

---
+++ src/constants/app-routes.js
@@ -0,0 +1,15 @@
+export const APP_ROUTES = {
+    private: {
+        about: {
+            name: '/about'
+        },
+        unauthorized: {
+            name: '/unauthorized'
+        }
+    },
+
+    public: {
+        home: '/',
+        forget_password: '/forget-password'
+    }
+};
